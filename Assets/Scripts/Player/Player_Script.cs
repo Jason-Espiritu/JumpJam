078173ad12bc9ef025d247a1ff1f43ba8554f2f9
Jason-Espiritu <jason_espiritu04@yahoo.com>
2022-02-21 13:30:26 +0800

    Added Sprites Level 1
    Added Additional Animation Script on Player.Cs

--- Assets/Scripts/Player/Player_Script.cs
+++ Assets/Scripts/Player/Player_Script.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_Script : MonoBehaviour
 {
     [SerializeField] private GameManager GM;
+    [SerializeField] private Animator PlayerAnimator;
     [SerializeField] private float _speedMultiplier;
     [SerializeField] private float _jumpForce = 22f;
     [SerializeField] private float _fallMultiplier = 5f;
@@ -13,7 +14,7 @@ public class Player_Script : MonoBehaviour
     [SerializeField] private float _checkRadius;
     [SerializeField] private LayerMask _GroundLayer;
     
-    private Rigidbody2D rb;
+    private Rigidbody2D _playerRigidBody;
     
     [SerializeField] private bool _isJumping;
 
@@ -23,10 +24,10 @@ public class Player_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _playerRigidBody = GetComponent<Rigidbody2D>();
         _speedMultiplier = GameManager.GMInstance.g_BPM_Multiplier;
 
-        rb.gravityScale *= _speedMultiplier;
+        _playerRigidBody.gravityScale *= _speedMultiplier;
         _jumpForce *= (_constJF * _speedMultiplier) + _constJF;
         _fallMultiplier *= _speedMultiplier;
         _peakjump *= _speedMultiplier;
@@ -36,15 +37,20 @@ public class Player_Script : MonoBehaviour
     private void FixedUpdate()
     {
         FallMultiplier(_isJumping);
+
     }
 
     public void Jump()
     {
         if (GroundCheck())
         {
-            rb.velocity = Vector2.up * _jumpForce;
+            _playerRigidBody.velocity = Vector2.up * _jumpForce;
             _isJumping = true;
+
+            PlayerAnimator.SetBool("Jumping", true); // Execute Jump Anim
         }
+
+        //Starts the Game when the Player pressed Jump
         if(!GM.g_isGameStarted && !AudioManager.instance.MusicSource.isPlaying)
         {
             AudioManager.instance.MusicSource.time = 0.7f;
@@ -65,16 +71,29 @@ public class Player_Script : MonoBehaviour
     }
     private void FallMultiplier(bool fromJump)
     {
-        if (!GroundCheck() && rb.velocity.y < _peakjump)
+        if (!GroundCheck() && _playerRigidBody.velocity.y < _peakjump)
         {
             if (fromJump)
             {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * _fallMultiplier * Time.fixedDeltaTime;
+                _playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * _fallMultiplier * Time.fixedDeltaTime;
             }
         }
-        if (rb.velocity.y == 0f)
+
+        if (_playerRigidBody.velocity.y == 0f)
         {
             _isJumping = false;
+        }
+
+        //Animation Executions
+        if (GroundCheck())
+        {
+            PlayerAnimator.SetBool("Falling", false);
+        }
+
+        if(_playerRigidBody.velocity.y < 0)
+        {
+            PlayerAnimator.SetBool("Falling", true); //Play Falling Animation
+            PlayerAnimator.SetBool("Jumping", false);
         }
     }
 }
