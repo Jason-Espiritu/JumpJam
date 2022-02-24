a8b07aebe65f5f3551f04f5964f2f5b6496f83ea
Klixt-082099 <art.joseph.nallatan.ramirez@gmail.com>
2022-02-24 14:07:38 +0800

    Changes Start Scene, Lobby, Level Selection and Level Info yung may story
    Sa start Scene ang nabago ko yung  title logo placeholder lang yun, sa lobby naman okay na play btn lang ang may function, sa level selection level 1 done may mga ilan nlang babaguhin once na kumpleto na nila requrements ko. sa may story board naman ganon din. connected na din sa scene: Level 1.

---
+++ Assets/ScriptzzZ/SceneForLvlInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneForLvlInfo : MonoBehaviour
+{
+public void LoadScene()
+{
+SceneManager.LoadScene("Level 1 info Scene");
+}
+
+
+
+
+  
+}
