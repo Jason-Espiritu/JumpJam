f08588bfa25b419d381a6f2e77d74475522ff71f
Klixt-082099 <art.joseph.nallatan.ramirez@gmail.com>
2022-03-02 23:56:13 +0800

    From start to StoryBoard
    1. yung sa Main menu natin yung btn ng option wala ko malagay, nadagdag lang yung new 3 level concepts

---
+++ Assets/ScriptzzZ/GotoBoyScoutLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GotoBoyScoutLevel : MonoBehaviour
+{
+public void LoadScene()
+{
+SceneManager.LoadScene("Boy Scout");
+}
+
+
+
+
+  
+}
---
+++ Assets/ScriptzzZ/GotoNinjaLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GotoNinjaLevel : MonoBehaviour
+{
+public void LoadScene()
+{
+SceneManager.LoadScene("Ninja");
+}
+
+
+
+
+  
+}
