5cb640cd0883921c90fad926c1be356841d244df
Klixt-082099 <art.joseph.nallatan.ramirez@gmail.com>
2022-03-13 23:10:02 +0800

    Done Level 1 to 10

---
+++ Assets/ScriptzzZ/GotoFrostLand.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GotoFrostLand : MonoBehaviour
+
+{
+public void LoadScene()
+{
+SceneManager.LoadScene("FrostLand");
+}
+
+
+
+
+  
+}
